a4a24aed7deef32411a11485fda4027f80419014
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-10-31 17:42:13 -0400

    Example testing interaction with DB and Documentation added

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+const Example = require('./models/exampleModel');
 
 app.use(express.json());
 
@@ -13,7 +14,17 @@ app.get('/example', (req, res) => {
     res.send('API REST | EXAMPLE');
 });
 
+app.post('/example', async(req, res) => {
+    try {
+        const example = await Example.create(req.body);
+        res.status(200).json(example);
+    } catch (error) {
+        console.log(error);
+        res.status(400).json(error);
+    }
+});
 
+mongoose.set('strictQuery', false);
 mongoose.connect('mongodb+srv://Les_Jailes:Les_Jailes123@boutiqueclothing.wsg961s.mongodb.net/API-REST?retryWrites=true&w=majority').
 then(() => {
     app.listen(port, () => {
@@ -22,4 +33,5 @@ then(() => {
     console.log('API REST | Database connected on MongoDB Atlas');
 }).catch(err => {
     console.log('API REST | Database connection error');
+    console.log(err);
 });
