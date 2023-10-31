ec2fb439cc800cd6193bc5669d3b652263c54aa3
Misax148 <axel.ayala.9412013@usalesiana.edu.bo>
2023-10-31 10:20:23 -0400

    Mongoose and conection with Database aplicated

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('API REST');
 });
 
-app.listen(port, () => {
-    console.log('API REST | Server started | Port 3000');
-  });      
+app.get('/example', (req, res) => {
+    res.send('API REST | EXAMPLE');
+});
+
+
+mongoose.connect('mongodb+srv://Les_Jailes:Les_Jailes123@boutiqueclothing.wsg961s.mongodb.net/API-REST?retryWrites=true&w=majority').
+then(() => {
+    app.listen(port, () => {
+        console.log('API REST | Server started | Port 3000');
+      });      
+    console.log('API REST | Database connected on MongoDB Atlas');
+}).catch(err => {
+    console.log('API REST | Database connection error');
+});
