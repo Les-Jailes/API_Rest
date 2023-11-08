bd767b5c627512eff44db406371b672c3c657f93
Elvis-Castro-H <elvis.castro.9330441@usalesiana.edu.bo>
2023-11-08 15:07:55 -0400

    Corrected order

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -13,17 +13,17 @@ const userSchema = mongoose.Schema(
             type: String,
             required: true,
         },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+        },
         password: {
             type: String,
             required: true,
         },
         gender: {
             type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            unique: true,
             required: true,
         },
         imagePath: {
