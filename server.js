7a9169f4ed821b2e5981769e747db2f01ddcd7f2
denisgandel <denis.gandarillas.9355205@usalesiana.edu.bo>
2023-11-11 15:32:46 -0400

    Test Environment

--- server.js
+++ server.js
@@ -11,9 +11,10 @@ const PORT = process.env.PORT;
 const MONGODB_URL = process.env.MONGODB_URL;
 const FRONTEND1 = process.env.FRONTEND1;
 const FRONTEND2 = process.env.FRONTEND2;
+const TEST_ENVIRONMENT = process.env.TEST_ENVIRONMENT
 
 let corsOptions = {
-    origin: [FRONTEND1, FRONTEND2],
+    origin: [FRONTEND1, FRONTEND2, TEST_ENVIRONMENT],
     optionsSuccessStatus: 200
 }
 //Cors
