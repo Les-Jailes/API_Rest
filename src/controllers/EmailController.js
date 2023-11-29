7f896158e5e31e4a4bf1c4a84da574f822bc0cb0
victor-villca <villcavictorleon@gmail.com>
2023-11-28 21:17:16 -0400

    send static email function creation

--- src/controllers/EmailController.js
+++ src/controllers/EmailController.js
@@ -1,9 +1,60 @@
+require('dotenv').config()
 const nodemailer = require('nodemailer');
 const Mailgen = require('mailgen');
 
+const EMAIL = process.env.EMAIL
+const PASSWORD = process.env.PASSWORD
 
-const sendEmail = (req, res) => {
-    res.status(200).json({hello: "hello"})
+const sendEmail =  (req, res) => {
+    const { userEmail } = req.body;
+    let config = {
+        service : 'gmail',
+        auth : {
+            user: EMAIL,
+            pass: PASSWORD
+        }
+    }
+    let transporter = nodemailer.createTransport(config);
+    let MailGenerator = new Mailgen({
+        theme: "default",
+        product : {
+            name: "Mailgen",
+            link : 'https://mailgen.js/'
+        }
+    })
+    let response = {
+        body: {
+            name : `Daily Tuition from ${userEmail}`,
+            intro: "Your bill has arrived!",
+            table : {
+                data : [
+                    {
+                        item : "Nodemailer Stack Book",
+                        description: "A Backend application",
+                        price : "$10.99",
+                    }
+                ]
+            },
+            outro: "Looking forward to do more business"
+        }
+    }
+
+    let mail = MailGenerator.generate(response)
+
+    let message = {
+        from : EMAIL,
+        to : EMAIL,
+        subject: "Place Order",
+        html: mail
+    }
+
+    transporter.sendMail(message).then(() => {
+        return res.status(201).json({
+            msg: "you should receive an email"
+        })
+    }).catch(error => {
+        return res.status(500).json({ error })
+    })
 }
 
 module.exports = {
